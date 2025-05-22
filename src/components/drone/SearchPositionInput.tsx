ec7abdbce798aa9fb545ccecca754b4ae065cad4
wu649782027 <15151851516@163.com>
2025-05-22 19:25:12 +0800

    egm96

--- src/components/drone/SearchPositionInput.tsx
+++ src/components/drone/SearchPositionInput.tsx
@@ -2,13 +2,16 @@ import {Input} from "@/components/ui/input.tsx";
 import {useEffect, useState} from "react";
 import {useAjax} from "@/lib/http.ts";
 import {useDebouncedValue} from "@/hooks/public/utils.ts";
-
+import * as egm96 from 'egm96-universal'
 const MAP_API_PREFIX = "/map/api/v1";
 
 const SearchPositionInput = () => {
   const [value, setValue] = useState("");
   const debouncedValue = useDebouncedValue(value, 300);
   const {get} = useAjax();
+  console.log('egm96');
+  // console.log(egm96.egm96ToEllipsoid(30.899981061,121.444283145,80));
+
 
   useEffect(() => {
     if (!debouncedValue) return;
